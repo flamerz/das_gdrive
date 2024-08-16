3ce9c8efa090a50f7cfad8776ecfa573752930ac
chandrasatria <chandra.invi@gmail.com>
2024-08-16 17:58:06 +0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
-from setuptools import setup, find_packages
+# TODO: Remove this file when  v15.0.0 is released
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name = "das_gdrive"
 
-# get version from __version__ variable in das_gdrive/__init__.py
-from das_gdrive import __version__ as version
+setup()
 
-setup(
-	name="das_gdrive",
-	version=version,
-	description="DAS Google Drive",
-	author="DAS",
-	author_email="DAS",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+
+
+# from setuptools import setup, find_packages
+
+# with open("requirements.txt") as f:
+# 	install_requires = f.read().strip().split("\n")
+
+# # get version from __version__ variable in das_gdrive/__init__.py
+# from das_gdrive import __version__ as version
+
+# setup(
+# 	name="das_gdrive",
+# 	version=version,
+# 	description="DAS Google Drive",
+# 	author="DAS",
+# 	author_email="DAS",
+# 	packages=find_packages(),
+# 	zip_safe=False,
+# 	include_package_data=True,
+# 	install_requires=install_requires
+# )
